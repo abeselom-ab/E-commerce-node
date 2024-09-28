9a433e817dee330420237d35a2b609f815bb8854
abeselom-ab <abeselomtesfay@gmail.com>
2024-09-28 11:32:12 +0300

    I updated index.js and created product controller

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require("path");
 const multer = require('multer');
+const cors = require('cors');
+const Auth = require('./auth');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 /* file uploading uising multer*/
 app.use(express.static("./src/storage/uploads/"));
@@ -15,12 +19,32 @@ const upload = multer({
 });
 
 const port = process.env.PORT || 5000;
-mongoose.connect('mongodb://localhost:27017/e-commerce');
+const userController=require('./src/controller/userController');
+const productController = require('./src/controller/productController');
+
+ mongoose.connect('mongodb://localhost:27017/e-commerce');
 app.get('/', (req, res) => {
   res.send('Hello, Node.js!');
 });
 
+/* products routing */
+app.get('/product/:id',productController.getOne);
+app.get('/products/getAll',productController.getAll);
+app.get('/products/getProducts',productController.getProducts);
+app.post('/products/search',productController.search);
+
+app.post('/product/add',upload.single('productImage'),productController.addProduct);
+app.delete('/product/deleteProduct/:id',productController.deleteProduct);
+app.put('/product/edit/:id',upload.single('productImage'),productController.updateProduct);
+
+/*users routing*/
+app.get('/user/getUsers',userController.getUsers);
+app.get('/user/:id',userController.getOne);
+app.post('/user/login',userController.login);
+app.post('/user/add',upload.single('profilePicture'),userController.addUser);
+app.delete('/user/deleteUser/:id',userController.deleteUser);
+app.put('/user/updateUser/:id',upload.single('profilePicture'),userController.updateUser);
 
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-  }); 
+  console.log(`Server is running on http://localhost:${port}`);
+}); 
---
+++ src/controller/productController.js
@@ -0,0 +1,63 @@
+const product=require('../model/product.js');
+module.exports={
+    async getAll(req,res){
+        let response= await product.find()//select * from product tabel
+        res.send(response);
+    },
+    async getProducts(req,res){
+        let response= await product.find({}, 'productName productImage')//select productName and productImage from product tabel
+        res.send(response);
+    },
+    async search(req,res){
+        console.log(req.body);
+        try {
+             let response= await product.find({productName:req.body.searchInput},  'productName productImage');
+            res.send(response);
+        } catch (error) {
+            res.send(error.message);
+        }
+       
+    },
+    async getOne(req,res){
+        let response = await product.find({_id:req.params.id});
+        res.send(response);
+    },
+    async addProduct(req,res){     
+      const {file}=req
+          if(req.file){
+            req.body.productImage=req.file.originalname;
+          }
+       console.log(req.body)
+        
+        try {
+           let response = await product.create(req.body);
+           console.log("Product Added Successfuly"+response);
+           res.send(response);
+        } catch (error) {
+            console.log(error);
+            res.send(error.massage);
+        }
+    },
+    async deleteProduct(req,res){
+        try {
+            let id=req.params.id;
+        let response = await product.findOneAndDelete({_id:id});
+        res.send(response);
+        } catch (error) {
+            res.send(error.massage);
+        }
+        
+    },
+    async updateProduct(req,res){
+        if(req.file){
+            req.body.productImage=req.file.originalname;
+          }
+        try {
+            let response =await product.updateOne({_id:req.params.id},req.body);
+            res.send(response);
+        } catch (error) {
+            res.send(error.message);
+        }
+        
+    }
+}
